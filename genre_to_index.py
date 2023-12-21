499cf7de1d42af1860b2b61e234977f91815a61b
yong-won-kim <qsc753949@gmail.com>
2023-12-21 23:24:50 +0930

    docs: add readme file

---
+++ genre_to_index.py
@@ -0,0 +1,23 @@
+genres = ['acoustic', 'afrobeat', 'alt-rock', 'alternative', 'ambient', 'anime', 'black-metal', 'bluegrass', 'blues', 'bossanova', 'brazil', 'breakbeat', 'british', 'cantopop', 'chicago-house', 'children', 'chill', 'classical', 'club', 'comedy', 'country', 'dance', 'dancehall', 'death-metal', 'deep-house', 'detroit-techno', 'disco', 'disney', 'drum-and-bass', 'dub', 'dubstep', 'edm', 'electro', 'electronic', 'emo', 'folk', 'forro', 'french', 'funk', 'garage', 'german', 'gospel', 'goth', 'grindcore', 'groove', 'grunge', 'guitar', 'happy', 'hard-rock', 'hardcore', 'hardstyle', 'heavy-metal', 'hip-hop', 'holidays', 'honky-tonk', 'house', 'idm', 'indian', 'indie', 'indie-pop', 'industrial', 'iranian', 'j-dance', 'j-idol', 'j-pop', 'j-rock', 'jazz', 'k-pop', 'kids', 'latin', 'latino', 'malay', 'mandopop', 'metal', 'metal-misc', 'metalcore', 'minimal-techno', 'movies', 'mpb', 'new-age', 'new-release', 'opera', 'pagode', 'party', 'philippines-opm', 'piano', 'pop', 'pop-film', 'post-dubstep', 'power-pop', 'progressive-house', 'psych-rock', 'punk', 'punk-rock', 'r-n-b', 'rainy-day', 'reggae', 'reggaeton', 'road-trip', 'rock', 'rock-n-roll', 'rockabilly', 'romance', 'sad', 'salsa', 'samba', 'sertanejo', 'show-tunes', 'singer-songwriter', 'ska', 'sleep', 'songwriter', 'soul', 'soundtracks', 'spanish', 'study', 'summer', 'swedish', 'synth-pop', 'tango', 'techno', 'trance', 'trip-hop', 'turkish', 'work-out', 'world-music']
+import pymysql
+db_username = 'root'
+db_password = '0000'
+db_host = 'localhost'
+db_name = 'music'
+connection = pymysql.connect(
+    host=db_host,
+    user=db_username,
+    password=db_password,
+    database=db_name
+)
+cursor = connection.cursor()
+
+new_column_name = 'genre_index'
+data_type = 'INT'
+alter_query = f"ALTER TABLE track ADD {new_column_name} {data_type};"
+cursor.execute(alter_query)
+print("genre_index 추가")
+for index, genre in enumerate(genres, start=1):
+    insert_query = f"UPDATE track SET {new_column_name} = {index} WHERE genre = '{genre}';"
+    cursor.execute(insert_query)
+connection.commit()
